df6811a765256a004365c29aa91af8b90c491d36
barruTech <tipwindi26@gmail.com>
2022-09-29 00:21:22 +0300

    gggg

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import useData from '../../hooks/useData'
 import { BiArrowBack} from "react-icons/bi";
-import { BsCamera, BsPencil} from "react-icons/bs";
+import { BsCamera, BsPencil, BsPlusLg} from "react-icons/bs";
 import './profile.css'
 import NewChat from '../account/message/NewChat';
 import ChangeStatus from '../../components/changeStatus/ChangeStatus';
@@ -41,6 +41,11 @@ const Profile = () => {
         }
     }
 
+    const today = new Date()
+    // const age = today.getFullYear() - pilgrim?.dob?.getFullYear()
+
+    // console.log('age', age)
+
     const [change, setChange] = useState(null)
 
     const pilgrimRef = doc(db, 'pilgrims', `${pilgrim?.id}`)
@@ -54,6 +59,85 @@ const Profile = () => {
             await updateDoc(pilgrimRef, {
             photo: url
         })
+        setFile(null)
+
+        } catch (error) {
+            setError(error.message)
+        }
+        setLoading(false)
+
+       
+    }
+
+    const handlePassport = async (e) => {
+        e.preventDefault()
+
+        setLoading(true)
+
+        try {
+            await updateDoc(pilgrimRef, {
+            passport: url
+        })
+        setFile(null)
+
+        } catch (error) {
+            setError(error.message)
+        }
+        setLoading(false)
+
+       
+    }
+
+    const [passNo, setPassNo] = useState(pilgrim?.passNo)
+    const [passIssue, setPassIssue] = useState(pilgrim?.passIssue)
+    const [passExpire, setPassExpire] = useState(pilgrim?.passExpire)
+    const [passPlace, setPassPlace] = useState(pilgrim?.passPlace)
+
+    const handlePassDetails = async (e) => {
+        e.preventDefault()
+
+        setLoading(true)
+
+        try {
+            await updateDoc(pilgrimRef, {
+            passNo,
+            passIssue,
+            passExpire,
+            passPlace
+        })
+        setChange(null)
+
+        } catch (error) {
+            setError(error.message)
+        }
+        setLoading(false)
+
+       
+    }
+
+    const [mName, setMName] = useState(pilgrim?.mName)
+    const [mGender, setMGender] = useState(pilgrim?.mGender)
+    const [mAge, setMAge] = useState(pilgrim?.mAge)
+    const [mPhone, setMPhone] = useState(pilgrim?.mPhone)
+    const [mEmail, setMEmail] = useState(pilgrim?.mEmail)
+    const [mRel, setMRel] = useState(pilgrim?.mRel)
+
+    const handleMahrem = async (e) => {
+        e.preventDefault()
+
+        setLoading(true)
+
+        try {
+            await updateDoc(pilgrimRef, {
+                mName,
+                mGender,
+                mAge,
+                mPhone,
+                mEmail,
+                mRel
+
+        })
+        setChange(null)
 
         } catch (error) {
             setError(error.message)
@@ -168,31 +252,99 @@ const Profile = () => {
                         <h4>Passport Details</h4>
                         <button className='btn_edit' onClick={() =>setChange(1)}><BsPencil/></button>
                         {change === 1 && <>                      
-                            <button className='btn_save_btn'>Save Changes</button>
+                            <button className='btn_save_btn' onClick={handlePassDetails}>{loading? <Loading/> : 'Save Changes'}</button>
                             <button className='btn_cancel' onClick={() =>setChange(null)}>CANCEL</button>
                          </>}
                     </div>
                     
                     <div className='pass_group'>
                         <span>Passport #</span>
-                        <h4>1252253</h4>
+                        {change === 1? 
+                            <input 
+                                type='text' 
+                                className='pass_input'
+                                name='passNo'
+                                value={passNo || ''}                            
+                                onChange={(e) =>setPassNo(e.target.value)}
+                                /> :
+                        <h4>{pilgrim?.passNo? pilgrim?.passNo : 'No data' }</h4>}
                     </div>
                     <div className='pass_group'>
                         <span>Date of Issue</span>
-                        <h4>1252253</h4>
+                        {change === 1? 
+                            <input 
+                                type='date' 
+                                name='passIssue'
+                                value={passIssue || ''}
+                                className='pass_input'
+                                onChange={(e) =>setPassIssue(e.target.value)}
+                                /> :
+                        <h4>{pilgrim?.passIssue? pilgrim?.passIssue : 'No data' }</h4>}
                     </div>
                     <div className='pass_group'>
                         <span>Expiring Date</span>
-                        <h4>1252253</h4>
+                        {change === 1? 
+                            <input type='date' 
+                            className='pass_input'
+                            name='passExpire'
+                            value={passExpire || ''}                            
+                            onChange={(e) =>setPassExpire(e.target.value)}
+                            /> :
+                        <h4>{pilgrim?.passExpire? pilgrim?.passExpire : 'No data' }</h4>}
                     </div>
                     <div className='pass_group'>
                         <span>Place of Issue</span>
-                        <h4>1252253</h4>
+                        {change === 1? 
+                            <input 
+                            type='text' 
+                            className='pass_input'
+                            name='passPlace'
+                            value={passPlace || ''}                            
+                            onChange={(e) =>setPassPlace(e.target.value)}
+                            /> :
+                        <h4>{pilgrim?.passPlace? pilgrim?.passPlace : 'No data' }</h4>}
                     </div>
                 </div>
                 <div className="pass_copy_doc">
                     <div className="pass_card">
-                        attach Passport
+                    { file?                         
+                        <img src={URL.createObjectURL(file)} alt="" />                                                 
+                        :
+                         pilgrim?.passport? <img src={pilgrim?.passport} alt="" /> 
+                        
+                        // cuMission? <h4 className='mission_logo'>{cuMission?.name[0]}</h4> : 
+                        // isAdmin? <h4 className='mission_logo'>{isAdmin?.name[0]}</h4>:
+                    : null}
+                    {!file || !pilgrim?.passport &&
+                        <label htmlFor="passport" className='profile_photo'>
+                            <input 
+                                type="file" 
+                                name='passport' 
+                                id="passport" style={{display: 'none'}}
+                                onChange={handleSelect}
+                            />
+                        <span className='attached_pass'><BsPlusLg/>Passport</span>
+                        </label>
+                        
+                        }                      
+                    </div>
+                    <div className="pass_card_btns"> 
+                     {error && <span className='error error_profile'>{error}<button onClick={() =>setError('')} className='btn_error'>x</button></span>}                      
+                        <label htmlFor="passport" className='prof'>
+                            <input 
+                                type="file" 
+                                name='passport' 
+                                id="passport" style={{display: 'none'}}
+                                onChange={handleSelect}
+                            />
+                        <span className='btn_edit_pass btn_change'>Change</span>
+                        </label>
+                        {!file && pilgrim?.passport &&
+                        <button className='btn_edit_pass'>Download</button>}
+                        {file && <>
+                        <button className='btn_edit_pass' onClick={handlePassport}>Save</button>
+                        <button className='btn_edit_pass' onClick={() => setFile(null)}>Cancel</button>                        
+                         </>}
                     </div>
                      
                 </div>
@@ -205,35 +357,92 @@ const Profile = () => {
                         <h4>Pilgrim's Mahrem Info</h4>
                         <button className='btn_edit' onClick={() =>setChange(2)}><BsPencil/></button>
                         {change === 2 && <> 
-                            <button className='btn_save_btn'>Save Changes</button>
+                            <button className='btn_save_btn' onClick={handleMahrem}>{loading? <Loading/> : 'Save Changes'}</button>
                             <button className='btn_cancel' onClick={() =>setChange(null)}>CANCEL</button>
                         </>}
                     </div>
                      
                     <div className="next_details_wrapper">
                         <div className='pass_group'>
-                            <span>Name</span>
-                            <h4>1252253</h4>
+                            <span>Name</span>                           
+                            {change === 2? 
+                                <input 
+                                type='text' 
+                                className='pass_input1'
+                                name='mName'
+                                value={mName || ''}                            
+                                onChange={(e) =>setMName(e.target.value)}
+                                /> :
+                            <h4>{pilgrim?.mName? pilgrim?.mName : 'No data' }</h4>}
+                            
                         </div>
                         <div className='pass_group'>
                             <span>Gender</span>
-                            <h4>1252253</h4>
+                            {change === 2? 
+                                <select                                
+                                className='pass_input1'
+                                name='mGender'
+                                value={mGender || ''}                            
+                                onChange={(e) =>setMGender(e.target.value)}
+                                >
+                                   <option value="">Select</option>
+                                   <option value="Male">Male</option>
+                                   <option value="Female">Female</option> 
+                                </select> :
+                            <h4>{pilgrim?.mGender? pilgrim?.mGender : 'No data' }</h4>}
                         </div>
                         <div className='pass_group'>
                             <span>Age</span>
-                            <h4>1252253</h4>
+                            {change === 2? 
+                                <input 
+                                type='number' 
+                                className='pass_input1'
+                                name='mAge'
+                                value={mAge || ''}                            
+                                onChange={(e) =>setMAge(e.target.value)}
+                                /> :
+                            <h4>{pilgrim?.mAge? pilgrim?.mAge : 'No data' }</h4>}
                         </div>
                         <div className='pass_group'>
                             <span>Phone</span>
-                            <h4>1252253</h4>
+                            {change === 2? 
+                                <input 
+                                type='text' 
+                                className='pass_input1'
+                                name='mPhone'
+                                value={mPhone || ''}                            
+                                onChange={(e) =>setMPhone(e.target.value)}
+                                /> :
+                            <h4>{pilgrim?.mPhone? pilgrim?.mPhone : 'No data' }</h4>}
                         </div> 
                         <div className='pass_group'>
                             <span>Email</span>
-                            <h4>1252253</h4>
+                            {change === 2? 
+                                <input 
+                                type='text' 
+                                className='pass_input1'
+                                name='mEmail'
+                                value={mEmail || ''}                            
+                                onChange={(e) =>setMEmail(e.target.value)}
+                                /> :
+                            <h4>{pilgrim?.mEmail? pilgrim?.mEmail : 'No data' }</h4>}
                         </div>
                         <div className='pass_group'>
                             <span>Relation</span>
-                            <h4>1252253</h4>
+                            {change === 2? 
+                                <select                                
+                                className='pass_input1'
+                                name='mRel'
+                                value={mRel || ''}                            
+                                onChange={(e) =>setMRel(e.target.value)}
+                                >
+                                   <option value="">Select</option>
+                                   <option value="Father">Father</option>
+                                   <option value="Father">Father</option> 
+                                   <option value="Brother">Brother</option> 
+                                   <option value="Groundfather">Groundfather</option> 
+                                </select> :
+                            <h4>{pilgrim?.mRel? pilgrim?.mRel : 'No data' }</h4>}
                         </div> 
                     </div>
                 </div>
